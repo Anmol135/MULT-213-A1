2df7891071f0ae169cae29c4d200573ed2b7a6c7
Anmolpreet Kaur <>
2023-01-18 00:22:32 -0600

    -Linked the right JSON file to the javascript file. -console.log the data to see the object. -still need to work on the loops

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import data from './test.json' assert { type: 'json' };
+import data from './pokedex.json' assert { type: 'json' };
 const output = document.getElementById("output");
-
+console.log(data);
 let html = "";
 for (let i = 0; i < data.length; i++) {
     html += `<p>${data[i].name}: ${data[i].value}</p>`;
