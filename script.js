00e554aa05623eceaffe3b572bb20b6dcf0eddee
Anmolpreet Kaur <>
2023-01-18 00:34:12 -0600

    -Fix the for loop according to the required conditions. -Looping through the whole pokemon object and getting out the results. -Make a div container and div class to organize data better. -Inside the div class make a header and a footer -Putting the image in the header section and all the characteristics of pokemon in the footer section. -Using innerHTML to output data.

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ import data from './pokedex.json' assert { type: 'json' };
 const output = document.getElementById("output");
 console.log(data);
 let html = "";
-for (let i = 0; i < data.length; i++) {
-    html += `<p>${data[i].name}: ${data[i].value}</p>`;
+for (let i = 0; i < data.pokemon.length; i++) {
+    html += `
+    <div id="container">
+<div class="pokemon">
+<header>
+<p><img src="${data.pokemon[i].img}"
+</header>
+<footer>
+<p>ID:${data.pokemon[i].id}
+</footer>
+</div>
+</div>
+
+
+
+
+
+
+`;
 }
 output.innerHTML = html;
