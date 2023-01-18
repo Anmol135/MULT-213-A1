6aae08b397db4dd34fdc669f16bf84dda0676798
Anmolpreet Kaur <>
2023-01-18 00:41:10 -0600

    -Getting all the characteristics out of the JSON file. -Displaying information in paragraph form.

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ for (let i = 0; i < data.pokemon.length; i++) {
 </header>
 <footer>
 <p>ID:${data.pokemon[i].id}
+<p>${data.pokemon[i].num}-${data.pokemon[i].name}
+<p>Type:${data.pokemon[i].type}
+<p>Height:${data.pokemon[i].height}
+<p>Weight:${data.pokemon[i].weight}
+<p>Candy:${data.pokemon[i].candy}
+<p>Candy-Count:${data.pokemon[i].candy_count}
+<p>Egg:${data.pokemon[i].egg}
+<p>Spawn-Chance:${data.pokemon[i].spawn_chance}
+<p>Average-Spawns:${data.pokemon[i].avg_spawns}
+<p>Multipliers:${data.pokemon[i].multipliers}
+<p>Weaknesses:${data.pokemon[i].weaknesses}
 </footer>
 </div>
 </div>
